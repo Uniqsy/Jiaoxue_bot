5fd0b9c1aa337452f5f3db4e4b97266fb2a11b87
Uniqsy <m15265723639@163.com>
2020-03-03 17:02:28 +0800

    sentmessage1 code

--- MyBot/sentmessage1.go
+++ MyBot/sentmessage1.go
@@ -26,5 +26,24 @@ func main() {
 		}
 	})
 
+	// Send a text-img message
+	s := bot.NewMessage(10000000, "group").
+		At("1232332333").
+		Text("嘤嘤嘤").
+		NewLine().
+		FaceByName("调皮").
+		Text("这是一个测试").
+		ImageBase64("img.jpg").
+		Send()
+
+	// Withdraw that message
+	if s.Err == nil {
+		bot.DeleteMessage(s.Result.MessageID)
+	}
+
+	// Send a stand-alone message (No need to call Send())
+	bot.NewMessage(10000000, "private").
+		Dice()
+
 	http.ListenAndServe("0.0.0.0:8443", nil)
 }
